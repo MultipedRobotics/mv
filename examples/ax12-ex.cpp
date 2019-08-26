709c7434f051054fda576a96cff6890380ee03b4
walchko <walchko@users.noreply.github.com>
2019-08-26 17:58:33 -0400

    up

--- examples/ax12-ex.cpp
+++ examples/ax12-ex.cpp
@@ -7,24 +7,7 @@
 using namespace std;
 
 int main() {
-//     srand(time(NULL));
-//     Dynamixel dxl(1); // 57600 baud
-//
-//     if (!dxl.open_serial("/dev/ttyS0")) {
-//         std::cerr << "Can not open serial device" << std::endl;
-//         return -1;
-//     }
-// //    dxl.set_baud_rate(1, 1);
-// //    return 0;
-//
-//     while (1) {
-//         int pos = rand()%100-50;
-//         std::cerr << dxl.set_goal_position(1, 512+pos) << std::endl;
-//         usleep(1000*500);
-//     }
-//
-//     dxl.close_serial();
-//     return -1;
+
     AX12 servo;
     // servo.open("/dev/ttyS0", 17);
 
